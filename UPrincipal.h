827e5336256b7b999e7d9f9a66a97c80dd07002c
Eduardo-David <eduardodavidlucaschoque36@gmail.com>
2024-11-27 15:40:25 -0400

    iniciando proyecto

---
+++ UPrincipal.h
@@ -0,0 +1,25 @@
+//---------------------------------------------------------------------------
+
+#ifndef UPrincipalH
+#define UPrincipalH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TGroupBox *GroupBox1;
+	TGroupBox *GroupBox2;
+	TButton *Button1;
+	void __fastcall Button1Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
