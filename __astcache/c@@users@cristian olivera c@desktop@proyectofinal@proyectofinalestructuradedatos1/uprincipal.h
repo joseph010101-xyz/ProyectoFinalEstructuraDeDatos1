996931b607dab4092cc197ed1f69858f394d446e
Criss010101 <cristianoliverao.c@gmail.com>
2024-12-03 14:39:43 -0400

    avance

--- __astcache/c@@users@cristian olivera c@desktop@proyectofinal@proyectofinalestructuradedatos1/uprincipal.h
+++ __astcache/c@@users@cristian olivera c@desktop@proyectofinal@proyectofinalestructuradedatos1/uprincipal.h
@@ -42,6 +42,7 @@ __published:	// IDE-managed Components
 	TButton *Salir;
 	TButton *LimpiarCaja;
 	TButton *LimpiarPlataforma;
+	TImage *Image4;
 	void __fastcall Button1Click(TObject *Sender);
 	void __fastcall FormCreate(TObject *Sender);
 	void __fastcall Button3Click(TObject *Sender);
