d077f1e04833a031e770e9a168e5fb0fada8b156
Criss010101 <cristianoliverao.c@gmail.com>
2024-12-14 21:30:13 -0400

    avance

--- Ticked.h
+++ Ticked.h
@@ -1,29 +1,9 @@
-#ifndef TicketH
-#define TicketH
-#include <vcl.h>
-#include <string>
+//---------------------------------------------------------------------------
 
-using namespace std;
-class Ticket {
-private:
-	int id;
-	string serviceType; // Tipo de servicio (Depósito, Retiro, etc.)
-	bool status;        // true = en espera, false = atendido
+#ifndef TickedH
+#define TickedH
+//---------------------------------------------------------------------------
+class Ticked{
 
-public:
-	Ticket();
-	Ticket(int id, string serviceType);
-
-
-	void SetID(int id);
-	int GetID();
-
-	void SetServiceType(string serviceType);
-	string GetServiceType();
-
-	void SetStatus(bool status);
-	bool GetStatus();
-
-    string ToString();
 };
 #endif
